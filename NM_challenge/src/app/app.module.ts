dc8266ff09e4505c0f12b9e46f636b5307d48e25
dullorb <thomlg@hotmail.com>
2022-04-24 17:10:38 -0500

    added angular routing

--- NM_challenge/src/app/app.module.ts
+++ NM_challenge/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StaticPageComponent } from './static-page/static-page.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StaticPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      {path: 'static-page', component: StaticPageComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
