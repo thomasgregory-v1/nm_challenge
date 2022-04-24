e37e869948eaa06769977a76777774087c9c1fd9
dullorb <thomlg@hotmail.com>
2022-04-24 14:27:17 -0500

    rudimentary api using mongoDB and mongoose

--- index.js
+++ index.js
@@ -2,12 +2,17 @@
 
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-const mongodb_database = 'mongodb://localhost:27017/mean_stack_setup'
-mongoose.connect(mongodb_database)
+const mongodb_database_URI = 'mongodb://localhost:27017'
+const mongodb_database_name = '/test'
+mongoose.connect(mongodb_database_URI + mongodb_database_name)
 
 mongoose.connection.on('connected', () => {
-  console.log('Connected to database ' + mongodb_database)
+  console.log(
+    'Connected to database ' + mongodb_database_URI + mongodb_database_name,
+  )
 })
 
 mongoose.connection.on('error', (err) => {
@@ -15,12 +20,37 @@ mongoose.connection.on('error', (err) => {
 })
 
 const app = express()
-
 const port = 3000
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.listen(port, () => {
   console.log('server started on port: ' + port)
 })
 
-app.use('/', (req, res, next) => {
-  res.send('Home page')
+app.get('/', (req, res, next) => {
+  res.send('Home page thing')
+})
+
+app.get('/mockAPI', (req, res, next) => {
+  res.json("{fake:'value'}")
+})
+
+const tableSchema = mongoose.Schema(
+  {
+    room: Number,
+    TableNumber: Number,
+    NumberOfSeats: Number,
+  },
+  { collection: 'gamingTable' },
+)
+
+const Table = mongoose.model('gamingTable', tableSchema)
+
+app.get('/APItest', async (req, res, next) => {
+  console.log('testing find with mongoose')
+  let table = await Table.find() //{ Room: { $eq: 1 } })
+  res.json(table)
 })
