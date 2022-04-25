6d2f2fb9440c186d53f37171ce591d70af4e2f8a
dullorb <thomlg@hotmail.com>
2022-04-25 13:17:22 -0500

    addded filter

--- NM_challenge/src/app/filtered-api-results/filtered-api-results.component.ts
+++ NM_challenge/src/app/filtered-api-results/filtered-api-results.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { APIService } from '../api.service';
-
+import {ElementRef, Pipe, PipeTransform, ViewChild} from '@angular/core';
+import { GameTable } from '../Interface_Table'
 
 @Component({
   selector: 'app-filtered-api-results',
@@ -10,10 +11,16 @@ import { APIService } from '../api.service';
 
 export class FilteredApiResultsComponent implements OnInit {
 
- 
+  @ViewChild('room_num', {static: true}) room_numElement: ElementRef;
+  room_num_val: string = ''
 
-  apiData = []
-  constructor(private api:APIService) { }
+  apiData : GameTable[] = []
+
+  constructor(
+      private api:APIService,
+      room_numElement: ElementRef) { 
+        this.room_numElement=room_numElement;
+      }
 
   ngOnInit() {
     this.api.getData().subscribe(
@@ -21,4 +28,15 @@ export class FilteredApiResultsComponent implements OnInit {
     )
   }
 
+}
+
+
+@Pipe({name:'roomFilter'})
+export class RoomFilterPipe implements PipeTransform {
+  transform(value: GameTable[], roomNumber:string) {     
+    if(isNaN(parseInt(roomNumber))){
+      return value
+    }
+    return value.filter(val=>val.Room == parseInt(roomNumber))
+  }      
 }
