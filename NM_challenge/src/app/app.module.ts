fb4bbbc979aa0ebdb174d0c041d865d778a7ea4e
dullorb <thomlg@hotmail.com>
2022-04-25 10:49:41 -0500

    added routing for filtered API

--- NM_challenge/src/app/app.module.ts
+++ NM_challenge/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, FormControl } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StaticPageComponent } from './static-page/static-page.component';
 import { RouterModule } from '@angular/router';
 import { RoomFilterPipe, TestFilteringComponent } from './test-filtering/test-filtering.component';
 import { FilteredApiResultsComponent } from './filtered-api-results/filtered-api-results.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
@@ -22,9 +23,11 @@ import { FilteredApiResultsComponent } from './filtered-api-results/filtered-api
     BrowserModule,
     FormsModule,
     AppRoutingModule,
+    HttpClientModule,
     RouterModule.forRoot([
       {path: 'static-page', component: StaticPageComponent},
-      {path: 'test-filtering', component: TestFilteringComponent}
+      {path: 'test-filtering', component: TestFilteringComponent},
+      {path: 'filtered-api-results', component: FilteredApiResultsComponent}
     ])
   ],
   providers: [],
